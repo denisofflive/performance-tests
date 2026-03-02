505d24943d527f9a77e9cf9959e85602c6eac15b
denisofflive <denisofflive@mail.ru>
2026-03-02 12:10:46 +0300

    Практическое задание: работа с HTTPX

--- httpx_open_deposit_account.py
+++ httpx_open_deposit_account.py
@@ -1,8 +1,10 @@
 import time
 
-import httpx  # Импортируем библиотеку HTTPX
+# Импортируем библиотеку httpx для отправки HTTP-запросов
+import httpx
 
-# Данные для создания пользователя
+# Формируем уникальный email пользователя, используя текущее время,
+# чтобы избежать дублирования адресов при многократном запуске скрипта
 create_user_payload = {
     "email": f"user.{time.time()}@example.com",
     "lastName": "string",
@@ -11,20 +13,26 @@ create_user_payload = {
     "phoneNumber": "string"
 }
 
-# Выполняем запрос на создание пользователя
+# Отправляем POST-запрос на создание нового пользователя
 create_user_response = httpx.post("http://localhost:8003/api/v1/users", json=create_user_payload)
+
+# Получаем JSON-данные из ответа сервера
 create_user_response_data = create_user_response.json()
 
-# Выводим полученные данные пользователя
-print("Create user response:", create_user_response_data)
-print("Status Code:", create_user_response.status_code)
-user_id = create_user_response.json()["user"]["id"]
-print("User ID:", user_id)
+# Формируем payload для открытия депозитного счета, используя ID вновь созданного пользователя
+open_deposit_account_payload = {
+    "userId": create_user_response_data["user"]["id"]
+}
 
-# Выполняем запрос на открытие аккаунта c использованием USER ID
-post_account_response = httpx.post("http://localhost:8003/api/v1/accounts/open-deposit-account/",
-                                   json={"userId": create_user_response.json()["user"]["id"]})
+# Открываем депозитный счёт, отправляя POST-запрос с указанным userID
+open_deposit_account_response = httpx.post(
+    "http://localhost:8003/api/v1/accounts/open-deposit-account",
+    json=open_deposit_account_payload
+)
 
-# Выводим полученные данные
-print("Open Account response:", post_account_response)
-print("Status Code:", post_account_response.status_code)
+# Парсим ответ сервера в виде JSON
+open_deposit_account_response_data = open_deposit_account_response.json()
+
+# Выводим содержимое ответа и статус-код HTTP
+print("Open deposit account response:", open_deposit_account_response_data)
+print("Open deposit account status code:", open_deposit_account_response.status_code)
