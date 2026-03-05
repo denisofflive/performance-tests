0ffa4a777a24ec7891864afa4ec1f25c6eae1a93
denisofflive <denisofflive@mail.ru>
2026-03-05 17:01:06 +0300

    Практика: Написание HTTP API клиента

--- clients/http/gateway/cards/client.py
+++ clients/http/gateway/cards/client.py
@@ -5,9 +5,17 @@ from httpx import Response
 from clients.http.client import HTTPClient
 
 
-class CreateCardsGatewayHTTPClient(TypedDict):
+class IssueVirtualCardRequestDict(TypedDict):
     """
-    Структура данных для создания виртуальной и физической карт.
+    Структура данных для выпуска виртуальной карты.
+    """
+    userId: str
+    accountId: str
+
+
+class IssuePhysicalCardRequestDict(TypedDict):
+    """
+    Структура данных для выпуска физической карты.
     """
     userId: str
     accountId: str
@@ -18,20 +26,20 @@ class CardsGatewayHTTPClient(HTTPClient):
     Клиент для взаимодействия с /api/v1/cards сервиса http-gateway.
     """
 
-    def issue_virtual_card_api(self, request: CreateCardsGatewayHTTPClient) -> Response:
+    def issue_virtual_card_api(self, request: IssueVirtualCardRequestDict) -> Response:
         """
-         Создание новой виртуальной карты.
+        Выпуск виртуальной карты.
 
-        :param request: Словарь с данными новой виртуальной карты.
+        :param request: Словарь с данными для выпуска виртуальной карты.
         :return: Ответ от сервера (объект httpx.Response).
         """
         return self.post("/api/v1/cards/issue-virtual-card", json=request)
 
-    def issue_physical_card_api(self, request: CreateCardsGatewayHTTPClient) -> Response:
+    def issue_physical_card_api(self, request: IssuePhysicalCardRequestDict) -> Response:
         """
-        Создание новой физической карты.
+        Выпуск физической карты.
 
-        :param request: Словарь с данными новой физической карты.
+        :param request: Словарь с данными для выпуска физической карты.
         :return: Ответ от сервера (объект httpx.Response).
         """
         return self.post("/api/v1/cards/issue-physical-card", json=request)
